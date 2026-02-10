351f8df01a9305e660223314015428f926e2be8e
Sandesh Shinde <152735801+Molza01@users.noreply.github.com>
2026-02-10 13:27:30 +0530

    change

--- server/server.js
+++ server/server.js
@@ -163,5 +163,6 @@ function handleUserLeave(socket, roomId, userId) {
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
